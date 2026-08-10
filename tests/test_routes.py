ac6389537411bb9671595b4da0597554c3692dd1
Rahmatullah7866 <khan.rahmatullah345@gmail.com>
2026-08-10 06:30:03 -0400

    Fix W292 and W293 whitespace on last line

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -22,7 +22,6 @@ BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
-
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -127,10 +126,9 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
     def test_read_an_account(self):
         """It should Read a single Account"""
-        account= self._create_accounts(1)[0]
+        account = self._create_accounts(1)[0]
         resp = self.client.get(
             f"{BASE_URL}/{account.id}", content_type="application/json"
         )
@@ -177,7 +175,6 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -190,7 +187,6 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-
 
     def test_cors_security(self):
         """It should return a CORS header"""
